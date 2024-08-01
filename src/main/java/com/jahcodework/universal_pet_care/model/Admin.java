77ccd21078862594de543724f0b90fc584a5e7bd
dr-scooby <hinurali@hotmail.com>
2024-08-01 13:51:30 -0700

    updated Admin class

--- src/main/java/com/jahcodework/universal_pet_care/model/Admin.java
+++ src/main/java/com/jahcodework/universal_pet_care/model/Admin.java
@@ -1,27 +1,18 @@
 package com.jahcodework.universal_pet_care.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Admin extends User{
 
+    @Id
     private long id;
-    private String firstname;
-    private String lastname;
-    private String gender;
-    private String phonenumber;
-    private String email;
-    private String password;
-    private String userType;
-    private boolean isEnabled; // is user active
+
 
 
 }
