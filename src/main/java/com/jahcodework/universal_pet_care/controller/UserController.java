c1da5ce0675f00a60e5324548b0b093c06bf77c7
dr-scooby <hinurali@hotmail.com>
2024-08-05 13:37:38 -0700

    added RestController annotation

--- src/main/java/com/jahcodework/universal_pet_care/controller/UserController.java
+++ src/main/java/com/jahcodework/universal_pet_care/controller/UserController.java
@@ -3,9 +3,11 @@ package com.jahcodework.universal_pet_care.controller;
 import com.jahcodework.universal_pet_care.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RequestMapping("/users")
+@RestController
 public class UserController {
     private final UserService userService;
 }
