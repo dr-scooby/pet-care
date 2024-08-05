74a3c83c091d867465b73baba44c39985a3c2cf4
dr-scooby <hinurali@hotmail.com>
2024-08-05 13:36:33 -0700

    updated UserController.java

--- src/main/java/com/jahcodework/universal_pet_care/controller/UserController.java
+++ src/main/java/com/jahcodework/universal_pet_care/controller/UserController.java
@@ -1,4 +1,11 @@
 package com.jahcodework.universal_pet_care.controller;
 
+import com.jahcodework.universal_pet_care.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
 }
