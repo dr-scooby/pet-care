4576ac70ae4e229f7de1fac6185aad9d74e74cba
dr-scooby <hinurali@hotmail.com>
2024-08-05 14:30:11 -0700

    updated UserController and UserService

--- src/main/java/com/jahcodework/universal_pet_care/service/UserService.java
+++ src/main/java/com/jahcodework/universal_pet_care/service/UserService.java
@@ -1,10 +1,13 @@
 package com.jahcodework.universal_pet_care.service;
 
 import com.jahcodework.universal_pet_care.model.User;
+import com.jahcodework.universal_pet_care.repository.UserRepo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private UserRepo userrepo;
 
 
     public void add(User user){
