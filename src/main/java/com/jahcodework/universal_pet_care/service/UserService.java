7baab9f7eb9424eb789fb2121fe5d972b29f98d6
dr-scooby <hinurali@hotmail.com>
2024-08-05 14:31:16 -0700

    added RequiredArgsConstructor to UserService

--- src/main/java/com/jahcodework/universal_pet_care/service/UserService.java
+++ src/main/java/com/jahcodework/universal_pet_care/service/UserService.java
@@ -2,9 +2,11 @@ package com.jahcodework.universal_pet_care.service;
 
 import com.jahcodework.universal_pet_care.model.User;
 import com.jahcodework.universal_pet_care.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private UserRepo userrepo;
