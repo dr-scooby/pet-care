0012aca2bb99d57425caef0260d8c57b199fe23f
dr-scooby <hinurali@hotmail.com>
2024-08-05 14:37:41 -0700

    updated UserRepo interface

--- src/main/java/com/jahcodework/universal_pet_care/repository/UserRepo.java
+++ src/main/java/com/jahcodework/universal_pet_care/repository/UserRepo.java
@@ -1,4 +1,7 @@
 package com.jahcodework.universal_pet_care.repository;
 
-public interface UserRepo {
+import com.jahcodework.universal_pet_care.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
 }
