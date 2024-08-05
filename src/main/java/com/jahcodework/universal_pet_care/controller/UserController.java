fc4ada13ef712cf20abac1e2061d7f8a58077e7a
dr-scooby <hinurali@hotmail.com>
2024-08-05 14:27:09 -0700

    updated UserController and UserService

--- src/main/java/com/jahcodework/universal_pet_care/controller/UserController.java
+++ src/main/java/com/jahcodework/universal_pet_care/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.jahcodework.universal_pet_care.controller;
 
+import com.jahcodework.universal_pet_care.model.User;
 import com.jahcodework.universal_pet_care.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     private final UserService userService;
+
+
+    public void add(@RequestBody User user){
+
+    }
 }
--- src/main/java/com/jahcodework/universal_pet_care/service/UserService.java
+++ src/main/java/com/jahcodework/universal_pet_care/service/UserService.java
@@ -1,4 +1,7 @@
 package com.jahcodework.universal_pet_care.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 }
