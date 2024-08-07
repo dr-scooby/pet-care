6de94d5c0c6500fd73683e44f874eaaf0af7e704
dr-scooby <hinurali@hotmail.com>
2024-08-06 17:35:13 -0700

    created class RegistrationRequest

--- src/main/java/com/jahcodework/universal_pet_care/factory/UserFactory.java
+++ src/main/java/com/jahcodework/universal_pet_care/factory/UserFactory.java
@@ -1,8 +1,9 @@
 package com.jahcodework.universal_pet_care.factory;
 
 import com.jahcodework.universal_pet_care.model.User;
+import com.jahcodework.universal_pet_care.request.RegistrationRequest;
 
 public interface UserFactory {
 
-    public User createUser();
+    public User createUser(RegistrationRequest rr);
 }
