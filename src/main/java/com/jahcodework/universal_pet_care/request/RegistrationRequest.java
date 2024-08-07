fff949dd0cd27644de3bb49e93df7ff8b562e529
dr-scooby <hinurali@hotmail.com>
2024-08-07 13:26:01 -0700

    added Data annotation to RegistrationRequest

--- src/main/java/com/jahcodework/universal_pet_care/request/RegistrationRequest.java
+++ src/main/java/com/jahcodework/universal_pet_care/request/RegistrationRequest.java
@@ -1,4 +1,20 @@
 package com.jahcodework.universal_pet_care.request;
 
+import jakarta.persistence.Column;
+import lombok.Data;
+
+@Data
 public class RegistrationRequest {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    @Column(name="mobile")
+    private String phoneNumber;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String userType;
+    private boolean isEnabled; // is user active
+    private String specialization;
 }
