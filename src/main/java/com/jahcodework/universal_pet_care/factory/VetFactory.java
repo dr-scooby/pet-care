29fa1075e0008bbdda2dfe6e7f1d7b4c797b38c8
dr-scooby <hinurali@hotmail.com>
2024-08-07 18:03:37 -0700

    updated code

--- src/main/java/com/jahcodework/universal_pet_care/factory/VetFactory.java
+++ src/main/java/com/jahcodework/universal_pet_care/factory/VetFactory.java
@@ -1,13 +1,23 @@
 package com.jahcodework.universal_pet_care.factory;
 
 import com.jahcodework.universal_pet_care.model.User;
+import com.jahcodework.universal_pet_care.repository.VetRepo;
 import com.jahcodework.universal_pet_care.request.RegistrationRequest;
+import com.jahcodework.universal_pet_care.service.user.UserAttributesMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class VetFactory {
+
+    private final VetRepo vetrepo;
+    private final UserAttributesMapper userattrmapper;
+
     public User createVet(RegistrationRequest rr) {
+
+
 
     }
 }
--- src/main/java/com/jahcodework/universal_pet_care/repository/VetRepo.java
+++ src/main/java/com/jahcodework/universal_pet_care/repository/VetRepo.java
@@ -1,4 +1,7 @@
 package com.jahcodework.universal_pet_care.repository;
 
-public interface VetRepo {
+import com.jahcodework.universal_pet_care.model.Vet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VetRepo extends JpaRepository<Vet, Integer> {
 }
