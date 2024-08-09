b55bbe206f7283664349472d120186e7c8d82953
dr-scooby <hinurali@hotmail.com>
2024-08-09 00:06:13 -0700

    Admin working, data saved to DB

--- src/main/java/com/jahcodework/universal_pet_care/factory/AdminFactory.java
+++ src/main/java/com/jahcodework/universal_pet_care/factory/AdminFactory.java
@@ -1,12 +1,27 @@
 package com.jahcodework.universal_pet_care.factory;
 
+import com.jahcodework.universal_pet_care.model.Admin;
 import com.jahcodework.universal_pet_care.model.User;
+import com.jahcodework.universal_pet_care.repository.AdminRepo;
 import com.jahcodework.universal_pet_care.request.RegistrationRequest;
+import com.jahcodework.universal_pet_care.service.user.UserAttributesMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AdminFactory {
+
+    private final AdminRepo adrepo;
+    private final UserAttributesMapper userattr;
+
+
     public User createAdmin(RegistrationRequest rr) {
-        return null;
+
+        Admin ad = new Admin();
+        userattr.setCommonAttributes(rr, ad);
+
+
+        return adrepo.save(ad);
     }
 }
--- src/main/java/com/jahcodework/universal_pet_care/repository/AdminRepo.java
+++ src/main/java/com/jahcodework/universal_pet_care/repository/AdminRepo.java
@@ -1,4 +1,7 @@
 package com.jahcodework.universal_pet_care.repository;
 
-public interface AdminRepo {
+import com.jahcodework.universal_pet_care.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepo extends JpaRepository<Admin, Long> {
 }
