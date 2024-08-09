d09677809fe4cb8944c5a36e2cc26ddf6a415ea0
dr-scooby <hinurali@hotmail.com>
2024-08-09 11:32:00 -0700

    updated User DTO class

--- src/main/java/com/jahcodework/universal_pet_care/dto/UserDTO.java
+++ src/main/java/com/jahcodework/universal_pet_care/dto/UserDTO.java
@@ -1,4 +1,20 @@
 package com.jahcodework.universal_pet_care.dto;
 
+import jakarta.persistence.Column;
+import lombok.Data;
+
+// Data used for gettter and setter from lombok
+@Data
 public class UserDTO {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String userType;
+    private boolean isEnabled; // is user active
+    private String specialization;
+
 }
