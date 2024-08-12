33180880f70bba966b527e3c84e75b82e9a9d4a3
dr-scooby <hinurali@hotmail.com>
2024-08-12 13:58:21 -0700

    updated Product

--- src/main/java/com/jahcodework/universal_pet_care/product/Product.java
+++ src/main/java/com/jahcodework/universal_pet_care/product/Product.java
@@ -1,5 +1,12 @@
 package com.jahcodework.universal_pet_care.product;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     private Long id;
