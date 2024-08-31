3ba48c58e011196314572b0395fbfb0c786d5f3c
dr-scooby <hinurali@hotmail.com>
2024-08-31 13:36:13 -0700

    added modelmapper to pom.xml

---
+++ src/main/java/com/jahcodework/universal_pet_care/dto/EntityConverter.java
@@ -0,0 +1,8 @@
+package com.jahcodework.universal_pet_care.dto;
+
+import org.modelmapper.ModelMapper;
+
+// convert the entity utility class
+public class EntityConverter<T, D> {
+    private ModelMapper model;
+}
