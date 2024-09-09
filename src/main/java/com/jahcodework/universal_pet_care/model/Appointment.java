66be42ed9f3b301b5a2de5baa4dfd977cb389cdb
dr-scooby <hinurali@hotmail.com>
2024-09-09 13:43:15 -0700

    added Appointment class

---
+++ src/main/java/com/jahcodework/universal_pet_care/model/Appointment.java
@@ -0,0 +1,12 @@
+package com.jahcodework.universal_pet_care.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Appointment {
+
+    private Long id;
+    private String reason;
+    private LocalDate date;
+    private LocalTime time;
+}
