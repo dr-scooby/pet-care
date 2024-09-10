93c2f709acd569746f8d416f69ec911546d310a8
dr-scooby <hinurali@hotmail.com>
2024-09-09 20:34:24 -0700

    created AppointmentStatus enum

---
+++ src/main/java/com/jahcodework/universal_pet_care/enumms/AppointmentStatus.java
@@ -0,0 +1,12 @@
+package com.jahcodework.universal_pet_care.enumms;
+
+public enum AppointmentStatus {
+    CANCELLED,
+    ON_GOING,
+    UO_COMING,
+    APPROVED,
+    NOT_APPROVED,
+    WAITING_FOR_APPROVAL,
+    PENDING,
+    COMPLETED
+}
