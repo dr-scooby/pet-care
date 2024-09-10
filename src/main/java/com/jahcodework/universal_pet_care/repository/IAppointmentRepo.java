603c7e23cdb30aa272a06c44adf1bf8b001dc414
dr-scooby <hinurali@hotmail.com>
2024-09-09 19:43:10 -0700

    updated code

--- src/main/java/com/jahcodework/universal_pet_care/repository/IAppointmentRepo.java
+++ src/main/java/com/jahcodework/universal_pet_care/repository/IAppointmentRepo.java
@@ -4,5 +4,5 @@ package com.jahcodework.universal_pet_care.repository;
 import com.jahcodework.universal_pet_care.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IAppointmentRepo extends JpaRepository<Appointment, Integer> {
+public interface IAppointmentRepo extends JpaRepository<Appointment, Long> {
 }
