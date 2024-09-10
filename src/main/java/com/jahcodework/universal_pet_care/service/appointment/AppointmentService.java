909d749f2cb716f1a6ccf7d5da2e60f11e52d80f
dr-scooby <hinurali@hotmail.com>
2024-09-10 13:40:16 -0700

    updated code

--- src/main/java/com/jahcodework/universal_pet_care/service/appointment/AppointmentService.java
+++ src/main/java/com/jahcodework/universal_pet_care/service/appointment/AppointmentService.java
@@ -3,6 +3,7 @@ package com.jahcodework.universal_pet_care.service.appointment;
 import com.jahcodework.universal_pet_care.model.Appointment;
 import com.jahcodework.universal_pet_care.model.User;
 import com.jahcodework.universal_pet_care.repository.AppointmentRepo;
+import com.jahcodework.universal_pet_care.repository.UserRepo;
 import com.jahcodework.universal_pet_care.request.AppointmentRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -26,10 +27,12 @@ public class AppointmentService implements IAppointmentService{
 
     private final AppointmentRepo apptrepo;
 
+    private final UserRepo userrepo;
 
     @Override
     public Appointment createAppointment(Appointment appt, Long senderid, Long recipientid) {
-       // Optional<User>
+       Optional<User> sender =userrepo.findById(senderid);
+
         return null;
     }
 
