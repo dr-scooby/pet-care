c4c959f21cb0792f113617fcbbc5cf2e4e907e6c
dr-scooby <hinurali@hotmail.com>
2024-09-09 18:22:04 -0700

    created IAppointmentRepo.java

---
+++ src/main/java/com/jahcodework/universal_pet_care/repository/IAppointmentRepo.java
@@ -0,0 +1,8 @@
+package com.jahcodework.universal_pet_care.repository;
+
+
+import com.jahcodework.universal_pet_care.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IAppointmentRepo extends JpaRepository<Appointment, Integer> {
+}
