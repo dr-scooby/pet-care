541d8f5b0bc8cc818e91e9b8c937bc61b2b8bb72
dr-scooby <hinurali@hotmail.com>
2024-09-11 13:27:03 -0700

    added ResourceNotFoundException.java

---
+++ src/main/java/com/jahcodework/universal_pet_care/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.jahcodework.universal_pet_care.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+
+        super(message);
+
+    }
+}
