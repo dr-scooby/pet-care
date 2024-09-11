1051e7db279183cf7e36513df715d660efb57672
dr-scooby <hinurali@hotmail.com>
2024-09-11 15:59:23 -0700

    updated code

--- src/main/java/com/jahcodework/universal_pet_care/repository/AppointmentRepo.java
+++ src/main/java/com/jahcodework/universal_pet_care/repository/AppointmentRepo.java
@@ -10,28 +10,6 @@ import java.time.LocalDate;
 
 public interface AppointmentRepo extends JpaRepository<Appointment, Long> {
 
-    /*
-     private Long id;
-    private String reason;
-    @Column(name="appointment_date")
-    private LocalDate date;
-    @Column(name="appointment_time")
-    private LocalTime time;
-    private String appointmentNo;
-    private LocalDate createdAt; // when this first created
-
-    @JoinColumn(name="sender")
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User patient;  // the originator, the patient the customer the client
-
-    @JoinColumn(name="recipient")
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User theVet; // the target of the appointment
-
-    @Enumerated(EnumType.STRING)
-    private AppointmentStatus status;
-     */
-
 
     Appointment findByAppointmentNo(String appointmentNo);
 
